7f2afb953fecc58541c4071a621b5e8aea463228
Valentin Stoev <valio_180@abv.bg>
2020-04-20 05:00:23 +0300

    Update Startup.cs
    Refactoring ConfigureServices

--- Server/Hangman.Server/Startup.cs
+++ Server/Hangman.Server/Startup.cs
@@ -15,16 +15,12 @@ namespace Hangman.Server
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDatabase(this.Configuration)
+        
+        public void ConfigureServices(IServiceCollection services) => services
+                    .AddDatabase(this.Configuration)
                     .AddIdentity()
                     .AddControllers();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
