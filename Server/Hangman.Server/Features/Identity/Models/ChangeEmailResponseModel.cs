a634867b8e255607865af896ab3e93aebff41da4
Valentin <Valio_180@abv.bg>
2020-05-07 14:21:29 +0300

    Add Models

---
+++ Server/Hangman.Server/Features/Identity/Models/ChangeEmailResponseModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hangman.Server.Features.Identity.Models
+{
+    public class ChangeEmailResponseModel
+    {
+        public string Token { get; set; }
+
+        public IEnumerable<IdentityError> Error { get; set; }
+    }
+}
---
+++ Server/Hangman.Server/Features/Identity/Models/ChangePasswordRequestModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hangman.Server.Features.Identity.Models
+{
+    public class ChangePasswordRequestModel
+    {
+        public string Password { get; set; }
+
+        public string NewPassword { get; set; }
+    }
+}
